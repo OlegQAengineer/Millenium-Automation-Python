c559e2356826cbe3d5c65f5193935e0cb956556a
Vartseli85 <Shcherbakovoleg085@gmail.com>
2022-12-09 17:53:38 -0800

    Working code

---
+++ Millenium/test_HomePage.py
@@ -0,0 +1,47 @@
+import time
+import unittest
+
+from selenium.webdriver.common.by import By
+
+
+from selenium.webdriver.support import expected_conditions as EC
+
+from faker import Faker
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+faker_class = Faker()
+
+
+class ChromeSearch(unittest.TestCase):
+
+    def setUp(self):
+
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver.maximize_window()
+
+        time.sleep(5)
+
+    def test_burgermenu(self):
+        driver = self.driver
+        driver.get("https://www.mlp.com/")
+        wait = WebDriverWait(driver, 3)
+
+        action = webdriver.ActionChains(driver)
+
+        action.move_to_element(driver.find_element(By.XPATH, "//button[contains(@class,'primary-menu')]")).click().perform()
+        wait.until(EC.visibility_of_element_located((By.ID, "menu-primary-menu")))
+        print("Nav menu open")
+        driver.get_screenshot_as_file('NavMenuOpen.png')
+
+        self.assertTrue(self, driver.find_element(By.ID, "menu-primary-menu"))
+
+
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+

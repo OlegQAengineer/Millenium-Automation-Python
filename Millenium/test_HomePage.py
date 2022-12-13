d34adfe882a0753c0c383e5ce77474d27110e184
Vartseli85 <Shcherbakovoleg085@gmail.com>
2022-12-12 21:51:47 -0800

    Last commit

--- Millenium/test_HomePage.py
+++ Millenium/test_HomePage.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 
 
 from selenium.webdriver.support import expected_conditions as EC
-from page_objects.home_page import HomePage as HP
+# from page_objects.home_pagePageObject import HomePage as HP
 from faker import Faker
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -24,19 +24,23 @@ class ChromeSearch(unittest.TestCase):
         time.sleep(5)
 
     def test_burgermenu(self):
+
         driver = self.driver
         driver.get("https://www.mlp.com/")
         wait = WebDriverWait(driver, 3)
-
         action = webdriver.ActionChains(driver)
-        HP.pushHumburger(self)
+        time.sleep(10)
 
-        #action.move_to_element(driver.find_element(By.XPATH, HP.hamburgerMenu)).click().perform()
-        wait.until(EC.visibility_of_element_located((By.ID, HP.navMenu)))
-        print("Nav menu open")
-        driver.get_screenshot_as_file('NavMenuOpen.png')
-
-        self.assertTrue(self, driver.find_element(By.ID, HP.navMenu))
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//body/div[@id='global_cookie_policy']/div[1]/div[1]/a[1]"))).click()
+        # HP.cookies.click()
+        # hp = HP()
+        # HP.pushHumburger(hp)
+        # action.move_to_element(driver.find_element(By.XPATH, HP.hamburgerMenu)).click().perform()
+        # # wait.until(EC.visibility_of_element_located(HP.navMenu))
+        # # print("Nav menu open")
+        # # driver.get_screenshot_as_file('NavMenuOpen.png')
+        #
+        # self.assertTrue(HP.navMenu)
 
     def tearDown(self):
         self.driver.quit()
